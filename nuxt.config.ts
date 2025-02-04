8d5aa43b3458a4c05247c5fd70ff0eb04d200a6e
onmomo <mail@christian-moser.ch>
2025-02-04 09:24:40 +0100

    Configure dev server to allow connections from Plex server and players

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,9 @@ export default {
   runtimeConfig: {
     logLevel: 'info'
   },
+  devServer: {
+    host: '0.0.0.0', // required to allow plex server and players to connect and poll from squeeze plex hub players
+  },
   vite: {
     server: {
       allowedHosts: [
