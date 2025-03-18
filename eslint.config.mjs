bb32b49e0f821960d70a1aadd3fc33241c23537a
onmomo <mail@christian-moser.ch>
2025-03-18 20:38:15 +0100

    Add ESLint configuration and scripts for linting TypeScript and Nuxt projects

---
+++ eslint.config.mjs
@@ -0,0 +1,16 @@
+import { createConfigForNuxt } from '@nuxt/eslint-config'
+
+export default createConfigForNuxt({
+  // options here
+})
+  .prepend(
+    // ...Prepend some flat configs in front
+  )
+  // Override some rules in a specific config, based on their name
+  .override('nuxt/typescript/rules', {
+    rules: {
+      // ...Override rules, for example:
+      '@typescript-eslint/ban-types': 'off'
+    }
+  })
+  // ...you can chain more operations as needed
