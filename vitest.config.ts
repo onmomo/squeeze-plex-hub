67d28d595450a3a7edfbc9d4679c7e0663b4795d
onmomo <mail@christian-moser.ch>
2025-06-21 23:59:58 +0200

    generate junit test report

---
+++ vitest.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vitest/config'
+
+export default defineConfig({
+  test: {
+    reporters: [
+      ['junit']
+    ],
+    outputFile: 'junit-test-report.xml',
+  },
+})
