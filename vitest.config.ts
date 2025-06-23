d22ac1f1f189e5978362ce709e2aa1f3e9581935
onmomo <mail@christian-moser.ch>
2025-06-23 09:07:34 +0200

    align with codecov docs

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     reporters: [['default'], ['junit']],
-    outputFile: 'junit-test-report.xml'
+    outputFile: 'test-report.junit.xml'
   }
 })
