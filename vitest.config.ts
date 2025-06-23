8aa5dadae2da86fd219aa17720cf6d777d6d7361
onmomo <mail@christian-moser.ch>
2025-06-23 09:51:23 +0200

    remove exclude again

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,9 +3,6 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     reporters: [['default'], ['junit']],
-    outputFile: 'test-report.junit.xml',
-    coverage: {
-      exclude: ['**/*.config.ts']
-    }
+    outputFile: 'test-report.junit.xml'    
   }
 })
