d41c0f9a803410a2203b7a9d39f8a27eec9c12fa
onmomo <mail@christian-moser.ch>
2025-07-01 09:44:41 +0200

    Implement runSqueezePlayersScanner function and add unit tests for player scanning functionality

--- server/plugins/squeezePlayersScanner.ts
+++ server/plugins/squeezePlayersScanner.ts
@@ -2,37 +2,38 @@ import { useScheduler } from '#scheduler'
 import useLogger from '../composables/useLogger'
 import { SqueezeServerStub, SqueezeServer } from 'lms-squeeze-rpc-x'
 import type { ServerInfo } from 'lms-discovery'
-
-const logger = useLogger('squeezePlayersScanner')
-const storage = useStorage('DISCOVERY')
-const scheduler = useScheduler()
+import { log } from 'winston'
 
 export default defineNitroPlugin(() => {
-  squeezePlayersScanner()
+  runSqueezePlayersScanner()
 })
 
 /**
- * Scans for LMS (Logitech Media Server) players on the network and stores them in the DISCOVERY storage.
+ * Scans for LMS (Lyron / Logitech Media Server) players on the network and stores them in the DISCOVERY storage.
  */
-function squeezePlayersScanner() {
+export function runSqueezePlayersScanner() {
+  const logger = useLogger('squeezePlayersScanner')
+  const storage = useStorage('DISCOVERY')
+  const scheduler = useScheduler()
   scheduler
-    .run(() => {
+    .run(async () => {
       logger.debug('Scanning for squeeze devices ..')
-      storage.getKeys('servers/').then(async (servers) => {
+      await storage.getKeys('servers/').then(async (servers) => {
         if (!servers) {
           logger.debug('No LMS found in storage, skipping')
           return
         }
 
         for (const key of servers) {
+          logger.debug(`Scanning LMS with key '${key}' ..`)
           const server = await storage.getItem<ServerInfo>(key)
           if (server) {
             logger.debug(`Looking for players from LMS '${server.name} (${server.ip})' ..`)
             const client = new SqueezeServerStub(`http://${server.ip}:${server.jsonPort || '9000'}`)
             const lms = new SqueezeServer(client)
 
-            const playerInfos = await lms.getPlayerInfosAsync()             
-            for (const playerInfo of playerInfos) {              
+            const playerInfos = await lms.getPlayerInfosAsync()
+            for (const playerInfo of playerInfos) {
               try {
                 logger.info(`Found player '${playerInfo.name}' (ID: '${playerInfo.playerid}', model: '${playerInfo.model}')`)
               } catch (error) {
@@ -42,6 +43,7 @@ function squeezePlayersScanner() {
 
             logger.info(`Found '${playerInfos.length}' squeeze players on LMS '${server.name} (${server.ip})'`)
             await storage.setItem(`players/${server.uuid}`, playerInfos)
+            logger.debug(`Stored player infos for LMS '${server.name}' (${server.ip})`)
           }
         }
       })
