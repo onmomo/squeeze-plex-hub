adef2cb70ea943d0385feba6e4cfd255b746b1d9
onmomo <mail@christian-moser.ch>
2025-09-07 21:53:50 +0200

    - Enhance playback routes with additional comments and improve error handling in skipPrevious functionality - skipPrevious test cases

--- server/routes/player/playback/skipPrevious.get.ts
+++ server/routes/player/playback/skipPrevious.get.ts
@@ -1,8 +1,8 @@
 import useLogger from '~/server/composables/useLogger'
 import usePlayerInfo from '~/server/composables/usePlayerInfo'
-import { getRequestHeader } from 'h3'
-import ExtendedSqueezePlayer from '~/server/lib/squeezePlayer'
 import { responseHeaders } from '~/server/lib/plexApi'
+import { eventHandler, getRequestHeader, setResponseHeaders, sendNoContent } from 'h3'
+import useSqueezePlayer from '~/server/composables/useSqueezePlayer'
 
 const logger = useLogger('playback.skipPrevious')
 
@@ -10,7 +10,7 @@ export default eventHandler(async (event) => {
   const targetClientIdentifier = getRequestHeader(event, 'X-Plex-Target-Client-Identifier')
   const clientIdentifier = getRequestHeader(event, 'X-Plex-Client-Identifier')
   const deviceName = getRequestHeader(event, 'X-Plex-Device-Name')
-
+  
   if (!targetClientIdentifier || !clientIdentifier || !deviceName) {
     logger.warn(
       `Missing required parameters ('X-Plex-Target-Client-Identifier', 'X-Plex-Client-Identifier', 'X-Plex-Device-Name' headers), got:`,
@@ -25,15 +25,15 @@ export default eventHandler(async (event) => {
   }
 
   try {
-    const { playerInfo, serverStub } = await usePlayerInfo(targetClientIdentifier)
-    const player = new ExtendedSqueezePlayer(serverStub, playerInfo)
+    const { playerInfo } = await usePlayerInfo(targetClientIdentifier)
+    const { player } = await useSqueezePlayer(targetClientIdentifier)
 
     await player.skipPrevious()
     logger.info(`Player '${targetClientIdentifier}' skipped to previous track`)
     setResponseHeaders(event, Object.fromEntries(responseHeaders(playerInfo.playerid, playerInfo.name).entries()))
     return sendNoContent(event, 200)
   } catch (error) {
-    logger.warn(`Error when skipping to previous track player '${targetClientIdentifier}'`, error)
+    logger.warn(`Error when skipping to previous track with player '${targetClientIdentifier}'`, error)
     return event.respondWith(
       new Response(`Player '${targetClientIdentifier}' not available to start playing, try again later`, { status: 404 })
     )
