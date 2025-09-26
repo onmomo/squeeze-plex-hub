8c5bccdfc90107bae3c93cf761006f6296b6a703
onmomo <mail@christian-moser.ch>
2025-09-26 23:14:40 +0200

    fix: update logging message in catchAll middleware for clarity test: add unit tests for catchAll middleware logging functionality

--- server/middleware/catchAll.ts
+++ server/middleware/catchAll.ts
@@ -1,9 +1,11 @@
 import useLogger from "../composables/useLogger"
+import { eventHandler } from 'h3'
 
 const logger = useLogger('catchAll')
 
 export default eventHandler(async (event) => {
-    logger.debug(`catchAll route triggered: ${event.method} ${event.path}`)    
+    // Log the incoming request method and path for investigation purposes
+    logger.debug(`Received request: ${event.method} ${event.path}`)    
   })
 
   
